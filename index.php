16c29f805a149580fc3fc32617b4fedf3eb00643
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-11-27 15:51:14 +0100

    exercice_php_p5E3

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+$month = array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novenvbre', 'Décembre');
+echo 'nous somme au mois de' . ' ' . $month[5];
+?>
